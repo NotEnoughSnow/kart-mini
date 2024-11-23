abf87a8cad5bff0155c5d0a299774971f0d29600
Amin Mraidi <amine.mraidi@outlook.com>
2024-11-23 15:06:13 +0100

    added akida verify

--- main.py
+++ main.py
@@ -4,21 +4,71 @@ import core.world.sim.drive_env as base_env
 from core.evaluate_module import Eval_module
 
 from akida_models.model_io import load_model
+import numpy as np
 
-def evaluate_akida():
+import core.snn_utils as SNN_utils
+
+import torch
+
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+
+def evaluate_akida(env):
 
     model_akida = load_model("../akida_models/SMRE_random.fbz")
     model_akida.summary()
 
+    obs = np.array([[
+        1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667,
+        1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667,
+        1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667,
+        1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667,
+        1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667,
+        1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667,
+        1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667,
+        1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667,
+        1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667,
+        1.16666667, 1.16666667, 1.16666667, 1.16666667, 1.16666667, 0.34192791,
+        1.07690352, 0.75227227, 0.31401242, 0.94941888, -3.75118563, 0.77804336,
+        0.99765169, 0.06849162
+    ]], dtype=np.float32)
 
-def evaluate_cpu():
-    env_name = base_env
-    track_type = "boxes"
-    track_name = "big_S"
+    threshold = torch.tensor(env.high)
+    shift = np.abs(env.low)
 
-    env_factory = EnvFactory(env_name)
+    obs_st = SNN_utils.generate_spike_trains(obs,
+                                             num_steps=50,
+                                             threshold=threshold,
+                                             shift=shift)
 
-    env = env_factory.createEnv(track_type, track_name, None)
+    obs_st_int = obs_st.astype(np.int8)
+
+    obs_reshaped = obs_st_int.reshape(50, 1, 1, 68)
+
+    print(obs_reshaped.shape)
+
+    logits = model_akida.predict(obs_reshaped)
+
+    print(logits)
+
+
+def verify_akida():
+
+    model_akida = load_model("../akida_models/SMRE_random.fbz")
+    model_akida.summary()
+
+    # Assuming model_akida is already loaded
+    device_name = "PCIe/NSoC_v2/0"  # This is the detected Akida device
+    model_akida.map(device_name=device_name)
+
+    # Optionally, print mapping details
+    print(f"Model mapped to device: {device_name}")
+
+
+
+
+def evaluate_cpu(env):
 
     actor_state = "./ppo_actor.pth"
 
@@ -30,11 +80,19 @@ def evaluate_cpu():
 
 def main():
 
-    model = None
+    env_name = base_env
+    track_type = "boxes"
+    track_name = "big_S"
 
-    evaluate_akida()
+    env_factory = EnvFactory(env_name)
 
-    #evaluate_cpu()
+    env = env_factory.createEnv(track_type, track_name, None)
+
+    #evaluate_akida(env=env)
+
+    verify_akida()
+
+    #evaluate_cpu(env=env)
 
 if __name__ == "__main__":
 
